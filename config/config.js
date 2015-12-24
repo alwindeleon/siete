d408640e56f59bf03848adf65dde8de5670e1da1
Alwin de Leon <bludeleon@gmail.com>
2015-12-24 18:23:05 +0800

    initial commit

---
+++ config/config.js
@@ -0,0 +1,34 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'siete'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/siete-development'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'siete'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/siete-test'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'siete'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/siete-production'
+  }
+};
+
+module.exports = config[env];
