68692f6184d71d3549fad61f02638942a8c4b242
Alwin de Leon <bludeleon@gmail.com>
2016-01-10 23:58:58 +0800

    added long polling

---
+++ public/js/adviser-app.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+  var socket = io();
+  var batch;
+  socket.on('connection',function(){
+    batch = prompt('which batch are you going to handle?');
+    $('#batch').text(batch);
+    socket.emit('getAdviserQueue',batch,function(data){
+      console.log("done cause: " + data);
+    });
+  });
+  socket.on('updateAdviserQueue',function(updatedBatch){
+    console.log("INSIDE");
+    console.log(updatedBatch);
+    //if queue is empty
+    if(updatedBatch === 0) {
+      $('#queue').empty();
+      $('<div/>', {
+          'id':'myDiv',
+          'class':'collection-item batch1',
+          'text':'QUEUE IS EMPTY HOORAY',
+      }).appendTo('#queue');
+      $('#numAdvisees').text('0');
+      return;
+    }
+    $('#numAdvisees').text(String(updatedBatch.length));
+    $('#queue').empty();
+    for(var i = 0; i < 5; i++){
+      $('<div/>', {
+          'id':'myDiv',
+          'class':'collection-item batch1',
+          'text':updatedBatch[i],
+      }).appendTo('#queue');
+    }
+  });
+
+  $('#next').click(function(){
+    socket.emit('dequeueAdviser',batch,5);
+  });
+
+});
---
+++ public/js/regteam-app.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  var socket = io();
+  var adviseBatch = $('#batchName');
+  var adviseName = $('#adviseName');
+
+  var enlistSections = $('#sections');
+  var enlistName = $('#enlistName');
+  var cn = $('#CN');
+
+  var modal = $('#modal1');
+  socket.on('controlNumber',function(controlNumber){
+    cn.text(controlNumber);
+  });
+
+  $('#close').click(function(){
+    modal.hide();
+    cn.text('loading. . .');
+  });
+  $('button#advise').click(function(){
+      socket.emit('enqueueAdviser', adviseName.val(), adviseBatch.val());
+      adviseName.val('');
+      adviseBatch.val('');
+      modal.fadeIn(500);
+  });
+
+  $('button#enlist').click(function(){
+      socket.emit('enqueueEnlister', enlistName.val(), enlistSections.val());
+      adviseName.val('');
+      adviseBatch.val(''); 
+      modal.fadeIn(500);
+  });
+});
